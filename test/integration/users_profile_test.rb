7c09f078607cce950e1e82db487b4cfb5d6f09f4
ike <ikeda.m.s.64@gmail.com>
2018-07-17 13:57:44 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -12,11 +12,22 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
-    assert_select 'h1>img.gravatar'
+  #確認　 assert_select 'h1>img.gravator'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
-    @user.microposts.paginate(page: 1).each do |micropost|
+    @user.microposts.paginate(page: 1) do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_select 'div.pagination', count:1
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
+  
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+  
 end
